437e7c8114ba96734d8ff4252b6441c534d96970
Henrik Hedegaard <henrik.hedegaard@gmail.com>
2025-03-28 10:03:43 +0100

    updated icons

--- src/Activity.vue
+++ src/Activity.vue
@@ -1,16 +1,18 @@
 <template>
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="100%"
+    height="100%"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="12" cy="12" r="10" />
+    <path
+      d="M22 12H18L15 21L9 3L6 12H2"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
   </svg>
 </template>
 
