82e7ad4dff9ab44e7e80849a954cbe1e2ac023b5
Henrik Hedegaard <henrik.hedegaard@gmail.com>
2025-03-28 23:21:05 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -3,30 +3,42 @@ import Alert from './Alert.vue'
 import AlignCenter from './AlignCenter.vue'
 import AlignJustify from './AlignJustify.vue'
 import AlignRight from './AlignRight.vue'
-import Attachment01 from './Attachment01.vue'
-import Attachment02 from './Attachment02.vue'
+import ArrowDown from './ArrowDown.vue'
+import ArrowDownNarrow from './ArrowDownNarrow.vue'
 import ArrowLeft from './ArrowLeft.vue'
 import ArrowLeftNarrow from './ArrowLeftNarrow.vue'
 import ArrowRight from './ArrowRight.vue'
 import ArrowRightNarrow from './ArrowRightNarrow.vue'
 import ArrowUp from './ArrowUp.vue'
 import ArrowUpNarrow from './ArrowUpNarrow.vue'
+import Attachment01 from './Attachment01.vue'
+import Attachment02 from './Attachment02.vue'
+import Backpack from './Backpack.vue'
+import BarChart01 from './BarChart01.vue'
+import Bell from './Bell.vue'
+import BellRing from './BellRing.vue'
+import Bold from './Bold.vue'
+import Book from './Book.vue'
+import Box from './Box.vue'
+import Building01 from './Building01.vue'
+import Calendar from './Calendar.vue'
 import Camera01 from './Camera01.vue'
 import Car01 from './Car01.vue'
 import Check from './Check.vue'
 import ChevronDown from './ChevronDown.vue'
-import ChevronUp from './ChevronUp.vue'
-import ChevronLeft from './ChevronLeft.vue'
-import ChevronRight from './ChevronRight.vue'
 import ChevronDownDouble from './ChevronDownDouble.vue'
-import ChevronUpDouble from './ChevronUpDouble.vue'
+import ChevronLeft from './ChevronLeft.vue'
 import ChevronLeftDouble from './ChevronLeftDouble.vue'
+import ChevronRight from './ChevronRight.vue'
 import ChevronRightDouble from './ChevronRightDouble.vue'
+import ChevronUp from './ChevronUp.vue'
+import ChevronUpDouble from './ChevronUpDouble.vue'
 import Clock from './Clock.vue'
 import Close from './Close.vue'
 import CloseCircle from './CloseCircle.vue'
 import CloseSquare from './CloseSquare.vue'
 import Code02 from './Code02.vue'
+import CoinDollar from './CoinDollar.vue'
 import Compass03 from './Compass03.vue'
 import Copy01 from './Copy01.vue'
 import Document from './Document.vue'
@@ -43,6 +55,8 @@ export {
   AlignCenter,
   AlignJustify,
   AlignRight,
+  ArrowDown,
+  ArrowDownNarrow,
   ArrowLeft,
   ArrowLeftNarrow,
   ArrowRight,
@@ -60,23 +74,23 @@ export {
   Box,
   Building01,
   Calendar,
-  Calendar01,
   Camera01,
   Car01,
   Check,
   ChevronDown,
-  ChevronUp,
-  ChevronLeft,
-  ChevronRight,
   ChevronDownDouble,
-  ChevronUpDouble,
+  ChevronLeft,
   ChevronLeftDouble,
+  ChevronRight,
   ChevronRightDouble,
+  ChevronUp,
+  ChevronUpDouble,
   Clock,
   Close,
   CloseCircle,
   CloseSquare,
   Code02,
+  CoinDollar,
   Compass03,
   Copy01,
   Document,
