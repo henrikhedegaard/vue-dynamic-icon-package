f6c8322e0387effed02225d79de10129d2af7f20
Henrik Hedegaard <henrik.hedegaard@gmail.com>
2025-03-31 09:00:02 +0200

    update

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+// In src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import IconsPage from '../views/IconsPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'icons',
+      component: IconsPage
+    }
+  ]
+})
+
+export default router
---
+++ src/types/dt-project-icons.d.ts
@@ -0,0 +1,5 @@
+declare module 'dt-project-icons' {
+  import { Component } from 'vue'
+  const icons: Record<string, Component>
+  export = icons
+}
