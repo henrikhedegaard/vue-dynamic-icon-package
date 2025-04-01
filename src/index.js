0f476bda52f7bc24b219de308e9e6f9fb4fe9fdf
Henrik Hedegaard <henrik.hedegaard@gmail.com>
2025-04-01 14:39:23 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,103 +1,13 @@
-import Activity from './Activity.vue'
-import Alert from './Alert.vue'
-import AlignCenter from './AlignCenter.vue'
-import AlignJustify from './AlignJustify.vue'
-import AlignRight from './AlignRight.vue'
-import ArrowDown from './ArrowDown.vue'
-import ArrowDownNarrow from './ArrowDownNarrow.vue'
-import ArrowLeft from './ArrowLeft.vue'
-import ArrowLeftNarrow from './ArrowLeftNarrow.vue'
-import ArrowRight from './ArrowRight.vue'
-import ArrowRightNarrow from './ArrowRightNarrow.vue'
-import ArrowUp from './ArrowUp.vue'
-import ArrowUpNarrow from './ArrowUpNarrow.vue'
-import Attachment01 from './Attachment01.vue'
-import Attachment02 from './Attachment02.vue'
-import Backpack from './Backpack.vue'
-import BarChart01 from './BarChart01.vue'
-import Bell from './Bell.vue'
-import BellRing from './BellRing.vue'
-import Bold from './Bold.vue'
-import Book from './Book.vue'
-import Box from './Box.vue'
-import Building01 from './Building01.vue'
-import Calendar from './Calendar.vue'
-import Camera01 from './Camera01.vue'
-import Car01 from './Car01.vue'
-import Check from './Check.vue'
-import ChevronDown from './ChevronDown.vue'
-import ChevronDownDouble from './ChevronDownDouble.vue'
-import ChevronLeft from './ChevronLeft.vue'
-import ChevronLeftDouble from './ChevronLeftDouble.vue'
-import ChevronRight from './ChevronRight.vue'
-import ChevronRightDouble from './ChevronRightDouble.vue'
-import ChevronUp from './ChevronUp.vue'
-import ChevronUpDouble from './ChevronUpDouble.vue'
-import Clock from './Clock.vue'
-import Close from './Close.vue'
-import CloseCircle from './CloseCircle.vue'
-import CloseSquare from './CloseSquare.vue'
-import Code02 from './Code02.vue'
-import CoinDollar from './CoinDollar.vue'
-import Compass03 from './Compass03.vue'
-import Copy01 from './Copy01.vue'
-import Document from './Document.vue'
-import DollarSymbol from './DollarSymbol.vue'
-import Download01 from './Download01.vue'
-import Download02 from './Download02.vue'
-import Envelope from './Envelope.vue'
-import Settings01 from './Settings01.vue'
-import Settings02 from './Settings02.vue'
+import Icon from './Icon.vue'
 
-export {
-  Activity,
-  Alert,
-  AlignCenter,
-  AlignJustify,
-  AlignRight,
-  ArrowDown,
-  ArrowDownNarrow,
-  ArrowLeft,
-  ArrowLeftNarrow,
-  ArrowRight,
-  ArrowRightNarrow,
-  ArrowUp,
-  ArrowUpNarrow,
-  Attachment01,
-  Attachment02,
-  Backpack,
-  BarChart01,
-  Bell,
-  BellRing,
-  Bold,
-  Book,
-  Box,
-  Building01,
-  Calendar,
-  Camera01,
-  Car01,
-  Check,
-  ChevronDown,
-  ChevronDownDouble,
-  ChevronLeft,
-  ChevronLeftDouble,
-  ChevronRight,
-  ChevronRightDouble,
-  ChevronUp,
-  ChevronUpDouble,
-  Clock,
-  Close,
-  CloseCircle,
-  CloseSquare,
-  Code02,
-  CoinDollar,
-  Compass03,
-  Copy01,
-  Document,
-  DollarSymbol,
-  Download01,
-  Download02,
-  Envelope,
-  Settings01,
-  Settings02,
+// For backward compatibility, you can keep the individual icon imports
+// or remove them if you're ready to fully migrate to the new approach
+
+export { Icon }
+
+// Export a default object for Vue plugin usage
+export default {
+  install(app) {
+    app.component('Icon', Icon)
+  },
 }
