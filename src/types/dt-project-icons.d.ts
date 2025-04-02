ca2715bacda336c4c5b84ffe42c3909b7c96ad4f
Henrik Hedegaard <henrik.hedegaard@gmail.com>
2025-04-02 09:58:48 +0200

    update

--- src/types/dt-project-icons.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'dt-project-icons' {
-  import { Component } from 'vue'
-  const icons: Record<string, Component>
-  export = icons
-}
---
+++ src/views/DemoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo">
+    <h1>This is the Demo Page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .demo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
